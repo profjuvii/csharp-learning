039c1e310ee832e7dfc45f70de2e9001f41e9331
Denys Bondarchuk <profjuvi@gmail.com>
2024-05-10 04:05:34 +0200

    Added folder homework01

---
+++ homework01/homework01/Calculator.cs
@@ -0,0 +1,18 @@
+﻿namespace homework01
+{
+	public class Calculator
+	{
+		private float _value1;
+		private float _value2;
+
+		// Constructor
+		public Calculator(float value1, float value2) { _value1 = value1; _value2 = value2; }
+
+		// Methods
+		public float Sum() => _value1 + _value2;
+		public float Diffrence() => _value1 - _value2;
+		public float Product() => _value1 * _value2;
+		public float Quotient() => _value2 != 0 ? _value1 / _value2 : float.NaN;
+    }
+}
+
